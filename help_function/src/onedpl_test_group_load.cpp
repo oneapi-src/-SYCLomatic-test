513b1037b5f74e88a74dc1fe6d645312e7e6743b
Abhilash Majumder <30946547+abhilash1910@users.noreply.github.com>
2024-04-03 22:19:52 +0530

    update  test

--- help_function/src/onedpl_test_group_load.cpp
+++ help_function/src/onedpl_test_group_load.cpp
@@ -12,7 +12,8 @@
 #include <iostream>
 
 
-bool helper_function(const int* ptr,const char* func_name){
+bool helper_validation_function(const int* ptr,const char* func_name){
+  // Used for validation of output and expected output sequences
   int expected[512];
   for (int i = 0; i < 512; ++i) {
       expected[i] = i;
@@ -38,20 +39,24 @@ bool test_load_blocked() {
 
   sycl::buffer<int, 1> buffer(data, 512);
   q.submit([&](sycl::handler &h) {
-    sycl::accessor dacc(buffer, h, sycl::read_write);
+    using workgroup_load = dpct::group::workgroup_load<128, BLOCK_LOAD_DIRECT, int>;
+    size_t temp_storage_size = workgroup_load::get_local_memory_size(128);
+    sycl::local_accessor<uint8_t, 1> tacc(sycl::range<1>(temp_storage_size), h);
+    sycl::accessor data_accessor(buffer, h, sycl::read_write);
     h.parallel_for(
         sycl::nd_range<3>(sycl::range<3>(1, 1, 128), sycl::range<3>(1, 1, 128)),
         [=](sycl::nd_item<3> item) {
           int thread_data[4];
-          auto *d = dacc.get_multi_ptr<sycl::access::decorated::yes>().get();
-          dpct::group::load_blocked<128>(item.get_local_linear_id(), d, thread_data);
+          auto *d = data_accessor.get_multi_ptr<sycl::access::decorated::yes>().get();
+          auto *tmp = tacc.get_multi_ptr<sycl::access::decorated::yes>().get();
+          group_load(tmp).load(item,item.get_local_linear_id(), d, thread_data);
         });
   });
   q.wait_and_throw();
 
   sycl::host_accessor data_accessor(buffer, sycl::read_only);
   const int *ptr = data_accessor.get_multi_ptr<sycl::access::decorated::yes>();
-  return helper_function(ptr,"test_load_blocked");
+  return helper_validation_function(ptr,"test_load_blocked");
 }
 
 bool test_load_striped() {
@@ -61,20 +66,24 @@ bool test_load_striped() {
 
   sycl::buffer<int, 1> buffer(data, 512);
   q.submit([&](sycl::handler &h) {
-    sycl::accessor dacc(buffer, h, sycl::read_write);
+    using workgroup_load = dpct::group::workgroup_load<128, BLOCK_LOAD_STRIPED, int>;
+    size_t temp_storage_size = workgroup_load::get_local_memory_size(128);
+    sycl::local_accessor<uint8_t, 1> tacc(sycl::range<1>(temp_storage_size), h);
+    sycl::accessor data_accessor(buffer, h, sycl::read_write);
     h.parallel_for(
         sycl::nd_range<3>(sycl::range<3>(1, 1, 128), sycl::range<3>(1, 1, 128)),
         [=](sycl::nd_item<3> item) {
           int thread_data[4];
-          auto *d = dacc.get_multi_ptr<sycl::access::decorated::yes>().get();
-          dpct::group::load_striped<128>(item.get_local_linear_id(), d, thread_data);
+          auto *d = data_accessor.get_multi_ptr<sycl::access::decorated::yes>().get();
+          auto *tmp = tacc.get_multi_ptr<sycl::access::decorated::yes>().get();
+          group_load(tmp).load(item, d, thread_data);
         });
   });
   q.wait_and_throw();
 
   sycl::host_accessor data_accessor(buffer, sycl::read_only);
   const int *ptr = data_accessor.get_multi_ptr<sycl::access::decorated::yes>();
-  return helper_function(ptr,"test_load_blocked");
+  return helper_validation_function(ptr,"test_load_blocked");
 }
 
 bool test_load_subgroup_striped() {
@@ -90,7 +99,7 @@ bool test_load_subgroup_striped() {
         [=](sycl::nd_item<3> item) {
           int thread_data[4];
           auto *d = dacc.get_multi_ptr<sycl::access::decorated::yes>().get();
-          dpct::group::load_subgroup_striped<128, item>(item, item.get_local_linear_id(), d, thread_data);
+          dpct::group::uninitialized_load_subgroup_striped<128>(item, d, thread_data);
         });
   });
   q.wait_and_throw();
