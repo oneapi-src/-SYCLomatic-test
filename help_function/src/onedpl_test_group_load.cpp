66ec770dae585389dcb7fc9eb81645888ed1174a
Abhilash Majumder <30946547+abhilash1910@users.noreply.github.com>
2024-04-09 13:12:44 +0530

    Update help_function/src/onedpl_test_group_load.cpp
    Co-authored-by: Yihan Wang <yihan.wang@intel.com>

--- help_function/src/onedpl_test_group_load.cpp
+++ help_function/src/onedpl_test_group_load.cpp
@@ -1,4 +1,5 @@
-// ====------ onedpl_test_group_load.cpp---------- -*- C++ -* ----===////
+// ====------ onedpl_test_group_load.cpp------------ *- C++ -* ----===//
+
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
