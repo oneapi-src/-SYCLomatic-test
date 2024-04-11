5878cb710dc241e0970505f9cfcbe25f500e1fd0
Abhilash Majumder <30946547+abhilash1910@users.noreply.github.com>
2024-04-11 12:41:06 +0530

    update review comments

--- help_function/src/onedpl_test_group_load.cpp
+++ help_function/src/onedpl_test_group_load.cpp
@@ -43,23 +43,28 @@ bool test_load_blocked_striped(dpct::group::load_algorithm T) {
     size_t temp_storage_size = workgroup_load::get_local_memory_size(128);
     sycl::local_accessor<uint8_t, 1> tacc(sycl::range<1>(temp_storage_size), h);
     sycl::accessor data_accessor(buffer, h, sycl::read_write);
+    int thread_data[4];
     h.parallel_for(
         sycl::nd_range<3>(sycl::range<3>(1, 1, 128), sycl::range<3>(1, 1, 128)),
         [=](sycl::nd_item<3> item) {
-          int thread_data[4];
           auto *d = data_accessor.get_multi_ptr<sycl::access::decorated::yes>().get();
           auto *tmp = tacc.get_multi_ptr<sycl::access::decorated::yes>().get();
-          group_load(tmp).load(item,item.get_local_linear_id(), d, thread_data);
+          group_load(tmp).load(item, d, thread_data);
+          // Write thread_data of each work item at index to the global buffer
+          int global_index = item.get_global_linear_id() * 4; // Each thread_data has 4 elements
+            for (int i = 0; i < 4; ++i) {
+                buffer[global_index + i] = thread_data[i];
+            }
         });
   });
   q.wait_and_throw();
-
+  
   sycl::host_accessor data_accessor(buffer, sycl::read_only);
   const int *ptr = data_accessor.get_multi_ptr<sycl::access::decorated::yes>();
-  return helper_validation_function(ptr,"test_load_blocked");
+  return helper_validation_function(ptr);
 }
 
-bool test_load_subgroup_striped() {
+bool test_load_subgroup_striped_standalone() {
   sycl::queue q;
   int data[512];
   for (int i = 0; i < 512; i++) data[i] = i;
@@ -73,16 +78,49 @@ bool test_load_subgroup_striped() {
           int thread_data[4];
           auto *d = dacc.get_multi_ptr<sycl::access::decorated::yes>().get();
           dpct::group::uninitialized_load_subgroup_striped<128>(item, d, thread_data);
+          // Write thread_data of each work item at index to the global buffer
+          int global_index = item.get_global_linear_id() * 4; // Each thread_data has 4 elements
+            for (int i = 0; i < 4; ++i) {
+                buffer[global_index + i] = thread_data[i];
+            }
         });
   });
   q.wait_and_throw();
 
   sycl::host_accessor data_accessor(buffer, sycl::read_only);
   const int *ptr = data_accessor.get_multi_ptr<sycl::access::decorated::yes>();
-  return helper_validation_function(ptr,"test_load_blocked");
+  return helper_validation_function(ptr);
 }
 
+bool test_load_blocked_striped_standalone(dpct::group::load_algorithm T) {
+  sycl::queue q;
+  int data[512];
+  for (int i = 0; i < 512; i++) data[i] = i;
+
+  sycl::buffer<int, 1> buffer(data, 512);
+  q.submit([&](sycl::handler &h) {
+    sycl::accessor dacc(buffer, h, sycl::read_write);
+    h.parallel_for(
+        sycl::nd_range<3>(sycl::range<3>(1, 1, 128), sycl::range<3>(1, 1, 128)),
+        [=](sycl::nd_item<3> item) {
+          int thread_data[4];
+          auto *d = dacc.get_multi_ptr<sycl::access::decorated::yes>().get();
+          dpct::group::load_blocked<128, T>(item, d, thread_data);
+          // Write thread_data of each work item at index to the global buffer
+          int global_index = item.get_global_linear_id() * 4; // Each thread_data has 4 elements
+            for (int i = 0; i < 4; ++i) {
+                buffer[global_index + i] = thread_data[i];
+            }
+        });
+  });
+  q.wait_and_throw();
+
+  sycl::host_accessor data_accessor(buffer, sycl::read_only);
+  const int *ptr = data_accessor.get_multi_ptr<sycl::access::decorated::yes>();
+  return helper_validation_function(ptr);
+}
 
 int main() {
-  return !(test_load_blocked_striped(BLOCK_LOAD_DIRECT) && test_load_blocked_striped(BLOCK_LOAD_STRIPED) && test_load_subgroup_striped());
+  return !(test_load_blocked_striped(dpct::group::load_algorithm::BLOCK_LOAD_DIRECT) && test_load_blocked_striped(dpct::group::load_algorithm::BLOCK_LOAD_STRIPED) && test_load_subgroup_striped_standalone()
+           && test_load_blocked_striped_standalone(dpct::group::load_algorithm::BLOCK_LOAD_STRIPED) && test_load_blocked_striped_standalone(dpct::group::load_algorithm::BLOCK_LOAD_DIRECT));
 }
