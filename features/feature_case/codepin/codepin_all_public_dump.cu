3692f3ab19c938befff247cf8c8d068083b22755
intwanghao <hao3.wang@intel.com>
2024-04-24 09:27:43 +0800

    fix
    Signed-off-by: intwanghao <hao3.wang@intel.com>

--- features/feature_case/codepin/codepin_all_public_dump.cu
+++ features/feature_case/codepin/codepin_all_public_dump.cu
@@ -1,4 +1,4 @@
-//===--- codepin_all_public_dump.cu -------------------------------*- CUDA -*---===//
+//===--- codepin_all_public_dump.cu ------------------------------*- CUDA -*---===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
