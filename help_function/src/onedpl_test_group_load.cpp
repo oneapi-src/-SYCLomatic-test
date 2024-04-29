d2ae0ee86f745232e6ce51ff85dfc5e139a9d8ee
Abhilash Majumder <30946547+abhilash1910@users.noreply.github.com>
2024-04-30 00:34:44 +0530

    update tests

--- help_function/src/onedpl_test_group_load.cpp
+++ help_function/src/onedpl_test_group_load.cpp
@@ -13,27 +13,9 @@
 #include <iostream>
 #include <oneapi/dpl/iterator>
 
-
-
-bool helper_validation_function(const int* ptr, const char* func_name) {
-
-  // Used for validation of output and expected output sequences
-  
-  for (int i = 0; i < 512; ++i) {
-    if (ptr[i] != i) {
-      std::cout << func_name <<" failed\n";
-      std::ostream_iterator<int> Iter(std::cout, ", ");
-      std::copy(ptr, ptr + 512, Iter);
-      std::cout << std::endl;
-      return false;
-    }
-  }
-
-  std::cout << func_name <<" pass\n";
-  return true;
-}
-
 bool test_load_blocked_striped(dpct::group::load_algorithm T) {
+  // Tests dpct::group::load_algorithm::BLOCK_LOAD_DIRECT & dpct::group::load_algorithm::BLOCK_LOAD_STRIPED 
+  // in its entirety as API functions
   sycl::queue q;
   oneapi::dpl::counting_iterator<int> count_it(0);
   sycl::buffer<int, 1> buffer(count_it, count_it + 512);
@@ -61,10 +43,22 @@ bool test_load_blocked_striped(dpct::group::load_algorithm T) {
   
   sycl::host_accessor data_accessor(buffer, sycl::read_only);
   const int *ptr = data_accessor.get_multi_ptr<sycl::access::decorated::yes>();
-  return helper_validation_function(ptr);
+  for (int i = 0; i < 512; ++i) {
+    if (ptr[i] != i) {
+      std::cout << func_name <<" failed\n";
+      std::ostream_iterator<int> Iter(std::cout, ", ");
+      std::copy(ptr, ptr + 512, Iter);
+      std::cout << std::endl;
+      return false;
+    }
+  }
+
+  std::cout <<" pass\n";
+  return true;
 }
 
 bool test_load_subgroup_striped_standalone() {
+  // Tests dpct::group::load_subgroup_striped as standalone method
   sycl::queue q;
   int data[512];
   for (int i = 0; i < 512; i++) data[i] = i;
@@ -80,8 +74,8 @@ bool test_load_subgroup_striped_standalone() {
           dpct::group::uninitialized_load_subgroup_striped<128>(item, d, thread_data);
           // Write thread_data of each work item at index to the global buffer
           int global_index = item.get_global_linear_id() * 4; // Each thread_data has 4 elements
-            for (int i = 0; i < 4; ++i) {
-                buffer[global_index + i] = thread_data[i];
+          for (int i = 0; i < 4; ++i) {
+                dacc[global_index + i] = thread_data[i];
             }
         });
   });
@@ -89,10 +83,30 @@ bool test_load_subgroup_striped_standalone() {
 
   sycl::host_accessor data_accessor(buffer, sycl::read_only);
   const int *ptr = data_accessor.get_multi_ptr<sycl::access::decorated::yes>();
-  return helper_validation_function(ptr);
+  int expected[512];
+  for (int i = 0; i < 128; i++) {
+    expected[4 * i + 0] = i;
+    expected[4 * i + 1] = 4 * i + 1;
+    expected[4 * i + 2] = 4 * i + 2;
+    expected[4 * i + 3] = 4 * i + 3;
+  }
+  for (int i = 0; i < 512; ++i) {
+    if (ptr[i] != expected[i]) {
+      std::cout << func_name <<" failed\n";
+      std::ostream_iterator<int> Iter(std::cout, ", ");
+      std::copy(ptr, ptr + 512, Iter);
+      std::cout << std::endl;
+      return false;
+    }
+  }
+
+  std::cout <<" pass\n";
+  return true;
 }
 
 bool test_load_blocked_striped_standalone(dpct::group::load_algorithm T) {
+  // Tests dpct::group::load_algorithm::BLOCK_LOAD_DIRECT & dpct::group::load_algorithm::BLOCK_LOAD_STRIPED 
+  // as standalone methods
   sycl::queue q;
   int data[512];
   for (int i = 0; i < 512; i++) data[i] = i;
@@ -109,7 +123,7 @@ bool test_load_blocked_striped_standalone(dpct::group::load_algorithm T) {
           // Write thread_data of each work item at index to the global buffer
           int global_index = item.get_global_linear_id() * 4; // Each thread_data has 4 elements
             for (int i = 0; i < 4; ++i) {
-                buffer[global_index + i] = thread_data[i];
+                dacc[global_index + i] = thread_data[i];
             }
         });
   });
@@ -117,10 +131,22 @@ bool test_load_blocked_striped_standalone(dpct::group::load_algorithm T) {
 
   sycl::host_accessor data_accessor(buffer, sycl::read_only);
   const int *ptr = data_accessor.get_multi_ptr<sycl::access::decorated::yes>();
-  return helper_validation_function(ptr);
+  for (int i = 0; i < 512; ++i) {
+    if (ptr[i] != i) {
+      std::cout << func_name <<" failed\n";
+      std::ostream_iterator<int> Iter(std::cout, ", ");
+      std::copy(ptr, ptr + 512, Iter);
+      std::cout << std::endl;
+      return false;
+    }
+  }
+
+  std::cout <<" pass\n";
+  return true;
 }
 
 int main() {
+  
   return !(test_load_blocked_striped(dpct::group::load_algorithm::BLOCK_LOAD_DIRECT) && test_load_blocked_striped(dpct::group::load_algorithm::BLOCK_LOAD_STRIPED) && test_load_subgroup_striped_standalone()
            && test_load_blocked_striped_standalone(dpct::group::load_algorithm::BLOCK_LOAD_STRIPED) && test_load_blocked_striped_standalone(dpct::group::load_algorithm::BLOCK_LOAD_DIRECT));
 }
