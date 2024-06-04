6aebd2f68d5afa9fb7aa4dd5a2a3b8f7d4876ee8
Wang, Yihan <yihan.wang@intel.com>
2024-06-04 09:18:10 +0800

    Add license
    Signed-off-by: Wang, Yihan <yihan.wang@intel.com>

--- features/feature_case/cub/cub_device_spmv.cu
+++ features/feature_case/cub/cub_device_spmv.cu
@@ -1,3 +1,12 @@
+// ====------ cub_device_spmv.cu------------------------- *- CUDA -* ------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//
+// ===----------------------------------------------------------------------===//
+
 #include <algorithm>
 #include <cstdlib>
 #include <cub/cub.cuh>
