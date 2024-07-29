7d65cc40e5dee03c685eeaed0742ee2f64c0e56d
Wang, Yihan <yihan.wang@intel.com>
2024-07-29 11:05:16 +0800

    [SYCLomatic #2054] Fix comment
    Signed-off-by: Wang, Yihan <yihan.wang@intel.com>

--- features/feature_case/asm/asm_mem.cu
+++ features/feature_case/asm/asm_mem.cu
@@ -1,4 +1,4 @@
-// ====------ asm_mem.cu ----------------------------------- *- CUDA -* ---===//
+// ===------- asm_mem.cu ----------------------------------- *- CUDA -* ---===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
