fb0345883599c374eb28829325b75b3210a9af84
Wang, Hao3 <hao3.wang@intel.com>
2024-08-22 12:47:22 +0800

    fix
    Signed-off-by: Wang, Hao3 <hao3.wang@intel.com>

--- behavior_tests/src/codepin-graph/do_test.py
+++ behavior_tests/src/codepin-graph/do_test.py
@@ -15,19 +15,29 @@ import shutil
 
 from test_utils import *
 
+def read_os_release():
+    try:
+        f = open('/etc/os-release', 'r')
+        content = f.read()
+        f.close()
+        return content
+    except FileNotFoundError:
+        return None
+
 def setup_test():
     change_dir(test_config.current_test)
     return True
 
 def migrate_test():
-    if (importlib.util.find_spec("graphviz") is None) or (shutil.which("dot") is None):
-        print("Dependency is not installed. Skip this test.")
-        return True
-    call_subprocess("codepin-report.py --instrumented-cuda-log=./src/CodePin_CUDA.json --instrumented-sycl-log=./src/CodePin_SYCL.json --generate-data-flow-graph")
-    if os.path.exists("./CodePin_DataFlowGraph.pdf"):
-      return True
-    print("Expected file CodePin_DataFlowGraph.pdf is not exists.")
-    return False
+    osinfo = read_os_release()
+    if osinfo is not None:
+      if ("Ubuntu" in osinfo) and ("22.04" in osinfo):
+        call_subprocess("codepin-report.py --instrumented-cuda-log=./src/CodePin_CUDA.json --instrumented-sycl-log=./src/CodePin_SYCL.json --generate-data-flow-graph")
+        if os.path.exists("./CodePin_DataFlowGraph.pdf"):
+          return True
+        print("Expected file CodePin_DataFlowGraph.pdf is not exists.")
+        return False
+    return True
 
 def build_test():
     return True
