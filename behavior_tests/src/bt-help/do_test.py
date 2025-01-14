86c1b1d8d18e6aa104a6fc2528137b323fd54e95
Teja Alaghari <teja.alaghari@intel.com>
2025-01-15 00:31:28 +0800

    Added BT for --help

--- behavior_tests/src/bt-help/do_test.py
+++ behavior_tests/src/bt-help/do_test.py
@@ -13,14 +13,44 @@ import sys
 
 from test_utils import *
 
+def compare_file_with_string(file_path, input_string):
+    try:
+        # Read the content of the file
+        with open(file_path, 'r') as file:
+            file_content = file.read()
+
+        # Compare the file content with the input string
+        if file_content == input_string:
+            return True
+
+    except FileNotFoundError:
+        print(f'Error: The file "{file_path}" does not exist.')
+    except Exception as e:
+        print(f'An unexpected error occurred: {e}')
+
+    return False
+
+def check_help_output(help_opts):
+    call_subprocess(test_config.CT_TOOL + ' --help')
+    if not compare_file_with_string('help_all.txt', test_config.command_output):
+        print('Output mismtach for option: --help')
+        return False
+
+    for opt in help_opts:
+        call_subprocess(test_config.CT_TOOL + ' --help=' + opt)
+        if not compare_file_with_string('help_' + opt + '.txt', test_config.command_output):
+            print('Output mismtach for option: --help=' + opt)
+            return False
+
+    return True
+
 def setup_test():
     change_dir(test_config.current_test)
     return True
 
 def migrate_test():
-
-    call_subprocess(test_config.CT_TOOL +" --help")
-    return is_sub_string("USAGE", test_config.command_output)
+    help_opts = ['advanced', 'basic']
+    return check_help_output(help_opts)
 
 def build_test():
     return True
